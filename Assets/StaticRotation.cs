c52ce8b9cc185a03bd6d6d9b71a97fa0a7b59c82
Anton Suka <a.sukachev@lancaster.ac.uk>
2019-11-01 11:24:34 +0000

    Oculus&Other libs dump

---
+++ Assets/StaticRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticRotation : MonoBehaviour
+{
+    Quaternion Rotation;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Rotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.rotation = Rotation;
+    }
+}
