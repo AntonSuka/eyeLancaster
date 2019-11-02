e829cbc1371d79bc685fdee7498cf176da8641c6
Anton Suka <a.sukachev@lancaster.ac.uk>
2019-11-02 18:22:08 +0000

    Gitignore fix

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class test : MonoBehaviour
+{
+    public int Testing;
+    public GameObject firestyearllabtest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firestyearllabtest.GetComponent<TextMeshPro>().text = "Example" + Testing;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
